b679787a3f7a605eec0b8546ef5a256f8c4ff210
Alexis <af.allendes@outlook.com>
2021-03-03 22:01:08 -0300

    Add resistor-color to Python track

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,17 @@
+def color_code(color) -> int:
+    return colors().index(color)
+
+
+def colors() -> list:
+    return [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
