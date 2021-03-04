d5183de513d486944907121496d05c62b1ee39d0
Alexis <af.allendes@outlook.com>
2021-03-04 19:03:08 -0300

    Update raindrops in Python track.

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -6,4 +6,4 @@ def convert(number:int):
         raindrop_sounds += 'Plang'
     if number % 7 == 0:
         raindrop_sounds += 'Plong'
-    return raindrop_sounds if len(raindrop_sounds) > 0 else str(number)
+    return raindrop_sounds or str(number)
