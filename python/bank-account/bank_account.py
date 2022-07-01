50d174c3fe58dda0bd5b02affbd7359c035071d5
Alexis Flores <afallendes@outlook.com>
2022-07-01 14:21:15 -0400

    Update bank_account solution (Python)

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,10 +1,14 @@
 import threading
 
-ACCOUNT_CLOSED_ERROR_MESSAGE = 'Account is closed'
-ACCOUNT_OPENED_ERROR_MESSAGE = 'Acoount is opened'
-AMOUNT_ERROR_MESSAGE = 'Amount is not valid'
-
 class BankAccount:
+
+    error_messages = [
+        'amount must be greater than 0',
+        'amount must be less than balance',
+        'account not open',
+        'account already open'
+    ]
+
     def __init__(self) -> None:
         self.__open = False
         self.__lock = threading.Lock()
@@ -12,31 +16,33 @@ class BankAccount:
     def get_balance(self) -> None:
         if self.__open:
             return self.__balance
-        raise ValueError(ACCOUNT_CLOSED_ERROR_MESSAGE)
+        raise ValueError(self.error_messages[2])
 
     def open(self) -> None:
         if self.__open:
-            raise ValueError(ACCOUNT_OPENED_ERROR_MESSAGE)
+            raise ValueError(self.error_messages[3])
         self.__open = True
         self.__balance = 0
 
     def deposit(self, amount:int) -> None:
         with self.__lock:
             if not (self.__open):
-                raise ValueError(ACCOUNT_CLOSED_ERROR_MESSAGE)
+                raise ValueError(self.error_messages[2])
             if not (amount > 0):
-                raise ValueError(AMOUNT_ERROR_MESSAGE)
+                raise ValueError(self.error_messages[0])
             self.__balance += amount
 
     def withdraw(self, amount:int) -> None:
         with self.__lock:
             if not (self.__open):
-                raise ValueError(ACCOUNT_CLOSED_ERROR_MESSAGE)
-            if not (amount > 0 and self.__balance >= amount):
-                raise ValueError(AMOUNT_ERROR_MESSAGE)
+                raise ValueError(self.error_messages[2])
+            if not (self.__balance >= amount):
+                raise ValueError(self.error_messages[1])
+            if not (amount > 0):
+                raise ValueError(self.error_messages[0])
             self.__balance -= amount
 
     def close(self) -> None:
         if not (self.__open):
-            raise ValueError(ACCOUNT_CLOSED_ERROR_MESSAGE)
+            raise ValueError(self.error_messages[2])
         self.__open = False
