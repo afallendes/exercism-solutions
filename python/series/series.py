d95ad44d0523cd9f6b0dc2192234cacfe244f977
Alexis Flores <afallendes@outlook.com>
2022-07-01 14:37:07 -0400

    Update series solution (Python)

--- python/series/series.py
+++ python/series/series.py
@@ -1,8 +1,17 @@
+from operator import le
+
+
 def slices(series:str, length:int) -> list:
-    if length > 0 and length <= len(series):
-        return [
-            series[slice(i, length + i, 1)]
-            for i in range(len(series) - length + 1)
-        ]
-    raise ValueError('Enter a valid length value.')
+    if len(series) == 0:
+        raise ValueError('series cannot be empty') 
+    if length == 0:
+        raise ValueError('slice length cannot be zero')
+    if length < 0:
+        raise ValueError('slice length cannot be negative')
+    if length > len(series):
+        raise ValueError('slice length cannot be greater than series length')
+    return [
+        series[slice(i, length + i, 1)]
+        for i in range(len(series) - length + 1)
+    ]
 
