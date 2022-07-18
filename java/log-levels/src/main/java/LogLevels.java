06bb7ce18dee76c77e93fda22a2ffd8f66e33e65
Alexis Flores <afallendes@outlook.com>
2022-07-17 22:16:03 -0400

    Add log-levels solution to Java track

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,15 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        return logLine.split(": ")[1].trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        String t = logLine.split(": ")[0].trim();
+        return t.substring(1, t.length() - 1).toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        return String.format("%s (%s)", message(logLine), logLevel(logLine));
     }
 }
