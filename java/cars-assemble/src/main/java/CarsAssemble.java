20e41887c5cf01f1a4bd4be2a7b4a597e65f09be
Alexis Flores <afallendes@outlook.com>
2022-07-21 21:44:33 -0400

    Add cars-assemble solution to Java track

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,26 @@
 public class CarsAssemble {
 
+    private final int carsPerDay = 221;
+
+    private double successRate(int speed) {
+        if (speed == 0) {
+            return 0;
+        } else if (speed <= 4) {
+            return 1.00;
+        } else if (speed <= 8) {
+            return 0.90;
+        } else if (speed == 9) {
+            return 0.80;
+        } else {
+            return 0.77;
+        }
+    }
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+        return carsPerDay * speed * successRate(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int)Math.floor(productionRatePerHour(speed) / 60);
     }
 }
