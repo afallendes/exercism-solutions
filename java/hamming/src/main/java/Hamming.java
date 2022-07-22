4654af4f18517b73e4f498f024cf15afcbe20408
Alexis Flores <afallendes@outlook.com>
2022-07-22 01:32:04 -0400

    Add hamming solution to Java track

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,9 +1,38 @@
 public class Hamming {
+
+    private String leftStrand;
+    private String rightStrand;
+
     public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        if (leftStrand.length() == 0 && rightStrand.length() > 0) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+
+        if (leftStrand.length() > 0 && rightStrand.length() == 0) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+
     }
 
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        int countDiffer = 0;
+
+        for (int i = 0; i < leftStrand.length(); i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                countDiffer++;
+            }
+        }
+
+        return countDiffer;
+
     }
 }
